35060cfe83ee056b44b38ff30c91a01dfb4567e8
Christine Brinchmann <christibr@nmbu.no>
2021-05-03 09:53:22 +0200

    cutting data + some changes on plotting the figure

--- plot_psd_data.py
+++ plot_psd_data.py
@@ -22,9 +22,27 @@ def get_data_filenames():
 def plot_psd_data(filename):
     # Loading data
     data = np.load('Data_PSD_crcns/'+filename)
+    f = np.array(data['f'][0, :])
+    psd = np.array(data['PSD'])
+    lw = 0.5
+    if 'hc2' in filename or 'pfc2' in filename:
+        # remove data below 0.1 Hz
+        index = np.where((f >= 0.1) & (f <= 300))
+        f = f[index]
+        psd = psd[index]
+        lw = 0.5
+    if 'ac2' in filename:
+        # remove data below 0.1 Hz
+        index = np.where((f <= 300))
+        f = f[index]
+        psd = psd[index]
+        lw = 1
+    if 'bf1' in filename:
+        lw = 1
+
     # Plotting psd vs. frequency
-    plt.plot(np.log10(data['f'][0, :]), np.log10(data['PSD']),
-             linewidth=0.5, label=filename[4:-4])
+    plt.plot(np.log10(f), np.log10(psd),
+             linewidth=lw, label=filename[4:-4])
 
 
 if __name__ == '__main__':
@@ -38,7 +56,6 @@ if __name__ == '__main__':
     plt.title('PSD of LFP data')
     plt.xlabel('log$_{10}$(frequency) [Hz]')
     plt.ylabel('log$_{10}$(PSD) [mV$^{2}$/Hz]')
-    plt.legend(loc="lower left", ncol=2)
-    # plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")  # outside right
-    plt.savefig('Figures/psd_of_lfp_data', dpi=500)
+    plt.legend(loc="lower left", ncol=2, prop={'size': 8.5})
+    plt.savefig('Figures/psd_of_lfp_data.pdf', dpi=500, bbox_inches='tight')
     plt.show()
