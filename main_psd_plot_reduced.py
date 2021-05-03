5887e17c12588baae03256942664772d3fa73052
Christine Brinchmann <christibr@nmbu.no>
2021-05-03 13:59:35 +0200

    added line colors for plotting and a reduced plot with fewer lines, and a zoomed in figure

--- main_psd_plot_reduced.py
+++ main_psd_plot_reduced.py
@@ -2,32 +2,35 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from plot_psd_data import get_data_filenames, plot_psd_data
-from plot_psd import psd_torbjorn, psd_graity, psd_from_miller2009, \
-    psd_from_milstein2009, psd_from_baranauskas2012, psd_from_jankowski2017
-
-# This file plots PSD from all cases in one figure
+from plot_psd import psd_torbjorn, psd_graity, \
+    psd_from_milstein2009, psd_from_baranauskas2012,\
+    psd_from_miller2009
 
 with_diff = True  # True = include diff, False = exclude diff
 with_SD = False  # True = include SD, False = exclude SD
+zoom = True  # zoomed figure
 
 plt.figure(figsize=(12, 7))
 
 # CRCNS data
 crcns_files = sorted(get_data_filenames(), reverse=True)
+index = [4, -4, -1]
 colors = ['cornflowerblue', 'gold', 'limegreen', 'darksalmon',
           'maroon', 'darkslategray',  'darkcyan', 'saddlebrown',
           'darkkhaki', 'lightgreen', 'skyblue', 'plum']
-for file, color in zip(crcns_files, colors):
+crcns_files_red = [crcns_files[i] for i in index]
+colors_red = [colors[i] for i in index]
+
+psd_torbjorn()
+
+for file, color in zip(crcns_files_red, colors_red):
     plot_psd_data(file, color)
 
 # other PSD/LFP data
-psd_torbjorn()
 psd_graity()
-psd_from_miller2009()
-#psd_from_milstein2009()
 psd_from_baranauskas2012()
-psd_from_jankowski2017()
-
+#psd_from_milstein2009()
+#psd_from_miller2009()
 # Diffusion data
 if with_diff:
     diff_data = pd.read_csv("Data_PSD_other/psd_data_normal.csv", index_col='f')
@@ -52,14 +55,24 @@ if with_SD:
 plt.title('PSD of diffusion potential, crcns data and other data')
 plt.xlabel('log$_{10}$(frequency) [Hz]')
 plt.ylabel('log$_{10}$(PSD) [mV$^{2}$/Hz]')
-# plt.xlim([-1, 2])
-# plt.ylim([-10, 1])
-# plt.legend(loc="lower left", ncol=2)
-plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left", ncol=2)  # outside right
+if with_diff and zoom:
+    plt.xlim([-1.1, 2])
+    plt.ylim([-9, -2])
+if with_SD and zoom:
+    plt.xlim([-1.1, 2])
+    plt.ylim([-7, 0])
 
-if with_diff:
-    plt.savefig('Figures/main_psd_plot.pdf', dpi=500, bbox_inches='tight')
-if with_SD:
-    plt.savefig('Figures/SD_main_psd_plot.pdf', dpi=500, bbox_inches='tight')
+# plt.legend(loc="lower left", ncol=2)
+plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")  # outside right
+
+if with_diff and zoom:
+    plt.savefig('Figures/main_REDZOOM_psd_plot.pdf', dpi=500, bbox_inches='tight')
+elif with_diff:
+    plt.savefig('Figures/main_RED_psd_plot.pdf', dpi=500, bbox_inches='tight')
+
+if with_SD and zoom:
+    plt.savefig('Figures/SD_main_REDZOOM_psd_plot.pdf', dpi=500, bbox_inches='tight')
+elif with_SD:
+    plt.savefig('Figures/SD_main_RED_psd_plot.pdf', dpi=500, bbox_inches='tight')
 
 plt.show()
