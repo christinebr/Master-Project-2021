bf9b4b6d7deb61e2a02814248ed78387547f50d6
Christine Brinchmann <christibr@nmbu.no>
2021-05-12 14:22:53 +0200

    changes figures (font size, legend, etc.)

--- main_psd_plot.py
+++ main_psd_plot.py
@@ -9,26 +9,26 @@ from plot_psd import psd_torbjorn, psd_graity, psd_from_miller2009, \
 
 with_diff = True  # True = include diff, False = exclude diff
 with_SD = False  # True = include SD, False = exclude SD
-
-plt.figure(figsize=(12, 7))
-
+plt.rc('font', size=13)
+plt.figure(figsize=(14, 10))
+psd_torbjorn()
 # CRCNS data
 crcns_files = sorted(get_data_filenames(), reverse=True)
 colors = ['cornflowerblue', 'gold', 'limegreen', 'darksalmon',
-          'maroon', 'darkslategray',  'darkcyan', 'saddlebrown',
+          'maroon', 'darkslategray', 'darkcyan', 'saddlebrown',
           'darkkhaki', 'lightgreen', 'skyblue', 'plum']
-for file, color in zip(crcns_files, colors):
-    plot_psd_data(file, color)
+abbr = ['A', 'B', 'A', 'B', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D']
+for file, color, abb in zip(crcns_files, colors, abbr):
+    plot_psd_data(file, color, abb)
 
 # other PSD/LFP data
-psd_torbjorn()
 psd_graity()
 psd_from_miller2009()
 #psd_from_milstein2009()
 psd_from_baranauskas2012()
 psd_from_jankowski2017()
 
-# Diffusion data
+# Normal data
 if with_diff:
     diff_data = pd.read_csv("Data_PSD_other/psd_data_normal.csv", index_col='f')
     columns = diff_data.columns
@@ -37,6 +37,8 @@ if with_diff:
     for col in columns:
         psd = diff_data[col].values
         plt.plot(np.log10(frequency), np.log10(psd), '--', label=col)
+
+    plt.title('PSD of diffusion potential, crcns data and other data')
 
 # Spreading depression
 if with_SD:
@@ -48,14 +50,15 @@ if with_SD:
         psd = SD_data[col].values
         plt.plot(np.log10(frequency), np.log10(psd), '-.', label=col)
 
-# Title, label and legend
-plt.title('PSD of diffusion potential, crcns data and other data')
+    plt.title('PSD of SD diffusion potential, crcns data and other data')
+# axis labels and legend
 plt.xlabel('log$_{10}$(frequency) [Hz]')
 plt.ylabel('log$_{10}$(PSD) [mV$^{2}$/Hz]')
 # plt.xlim([-1, 2])
 # plt.ylim([-10, 1])
 # plt.legend(loc="lower left", ncol=2)
-plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left", ncol=2)  # outside right
+# plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left", ncol=2)  # outside right
+plt.legend(bbox_to_anchor=(0.5, -0.08), loc="upper center", ncol=5)  # outside right
 
 if with_diff:
     plt.savefig('Figures/main_psd_plot.pdf', dpi=500, bbox_inches='tight')
--- main_psd_plot_reduced.py
+++ main_psd_plot_reduced.py
@@ -2,35 +2,34 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from plot_psd_data import get_data_filenames, plot_psd_data
-from plot_psd import psd_torbjorn, psd_graity, \
-    psd_from_milstein2009, psd_from_baranauskas2012,\
-    psd_from_miller2009
+from plot_psd import psd_torbjorn, psd_graity, psd_from_baranauskas2012
 
 with_diff = True  # True = include diff, False = exclude diff
 with_SD = False  # True = include SD, False = exclude SD
 zoom = True  # zoomed figure
-
+plt.rc('font', size=13)
 plt.figure(figsize=(12, 7))
 
 # CRCNS data
 crcns_files = sorted(get_data_filenames(), reverse=True)
 index = [4, -4, -1]
 colors = ['cornflowerblue', 'gold', 'limegreen', 'darksalmon',
-          'maroon', 'darkslategray',  'darkcyan', 'saddlebrown',
+          'maroon', 'darkslategray', 'darkcyan', 'saddlebrown',
           'darkkhaki', 'lightgreen', 'skyblue', 'plum']
+abbr = ['A', 'B', 'A', 'B', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D']
+
 crcns_files_red = [crcns_files[i] for i in index]
 colors_red = [colors[i] for i in index]
-
+abbr_red = [abbr[i] for i in index]
 psd_torbjorn()
 
-for file, color in zip(crcns_files_red, colors_red):
-    plot_psd_data(file, color)
+for file, color, abb in zip(crcns_files_red, colors_red, abbr_red):
+    plot_psd_data(file, color, abb)
 
 # other PSD/LFP data
 psd_graity()
 psd_from_baranauskas2012()
-#psd_from_milstein2009()
-#psd_from_miller2009()
+
 # Diffusion data
 if with_diff:
     diff_data = pd.read_csv("Data_PSD_other/psd_data_normal.csv", index_col='f')
@@ -40,6 +39,7 @@ if with_diff:
     for col in columns:
         psd = diff_data[col].values
         plt.plot(np.log10(frequency), np.log10(psd), '--', label=col)
+    plt.title('PSD of diffusion potential, crcns data and other data')
 
 # Spreading depression
 if with_SD:
@@ -50,9 +50,9 @@ if with_SD:
     for col in columns:
         psd = SD_data[col].values
         plt.plot(np.log10(frequency), np.log10(psd), '-.', label=col)
+    plt.title('PSD of SD diffusion potential, crcns data and other data')
 
-# Title, label and legend
-plt.title('PSD of diffusion potential, crcns data and other data')
+# axis labels and legend
 plt.xlabel('log$_{10}$(frequency) [Hz]')
 plt.ylabel('log$_{10}$(PSD) [mV$^{2}$/Hz]')
 if with_diff and zoom:
@@ -63,7 +63,7 @@ if with_SD and zoom:
     plt.ylim([-7, 0])
 
 # plt.legend(loc="lower left", ncol=2)
-plt.legend(bbox_to_anchor=(1.04, 1), loc="upper left")  # outside right
+plt.legend(bbox_to_anchor=(1.04, 1.05), loc="upper left")  # outside right
 
 if with_diff and zoom:
     plt.savefig('Figures/main_REDZOOM_psd_plot.pdf', dpi=500, bbox_inches='tight')
