a6013531e1462fc127ddae6f25962ce1ce904a93
Christine Brinchmann <christibr@nmbu.no>
2021-05-12 14:25:34 +0200

    changed legend names

--- plot_psd_data.py
+++ plot_psd_data.py
@@ -19,20 +19,20 @@ def get_data_filenames():
     return datafiles
 
 
-def plot_psd_data(filename, line_color):
+def plot_psd_data(filename, line_color, name):
     # Loading data
     data = np.load('Data_PSD_crcns/'+filename)
     f = np.array(data['f'][0, :])
     psd = np.array(data['PSD'])
     lw = 0.5
     if 'hc2' in filename or 'pfc2' in filename:
-        # remove data below 0.1 Hz
+        # remove data below 0.1 Hz and over 300 Hz
         index = np.where((f >= 0.1) & (f <= 300))
         f = f[index]
         psd = psd[index]
         lw = 0.5
     if 'ac2' in filename:
-        # remove data below 0.1 Hz
+        # remove data over 300 Hz
         index = np.where((f <= 300))
         f = f[index]
         psd = psd[index]
@@ -42,7 +42,7 @@ def plot_psd_data(filename, line_color):
 
     # Plotting psd vs. frequency
     plt.plot(np.log10(f), np.log10(psd), color=line_color,
-             linewidth=lw, label=filename[4:-4])
+             linewidth=lw, label=filename[4:8]+'_'+name)
 
 
 if __name__ == '__main__':
@@ -51,14 +51,15 @@ if __name__ == '__main__':
     # Plotting
     plt.figure()
     data_files = sorted(data_files, reverse=True)
+    abbr = ['A', 'B', 'A', 'B', 'A', 'B', 'C', 'D', 'A', 'B', 'C', 'D']
     colors = ['cornflowerblue', 'gold', 'limegreen', 'darksalmon',
               'maroon', 'darkslategray',  'darkcyan', 'saddlebrown',
               'darkkhaki', 'lightgreen', 'skyblue', 'plum']
-    for data_file, color in zip(data_files, colors):
-        plot_psd_data(data_file, line_color=color)
+    for data_file, color, abb in zip(data_files, colors, abbr):
+        plot_psd_data(data_file, line_color=color, name=abb)
     plt.title('PSD of LFP data')
     plt.xlabel('log$_{10}$(frequency) [Hz]')
     plt.ylabel('log$_{10}$(PSD) [mV$^{2}$/Hz]')
-    plt.legend(loc="lower left", ncol=2, prop={'size': 8.5})
+    plt.legend(loc="lower left", ncol=3, prop={'size': 9.0})
     plt.savefig('Figures/psd_of_lfp_data.pdf', dpi=500, bbox_inches='tight')
     plt.show()
