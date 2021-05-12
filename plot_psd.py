70fed25be99015a83f48f6d27ef8528bbf989fc3
Christine Brinchmann <christibr@nmbu.no>
2021-05-12 14:35:44 +0200

    updated linearregression with if test, removed milstein

--- plot_psd.py
+++ plot_psd.py
@@ -17,7 +17,8 @@ def psd_torbjorn(do_linreg=False):
     if do_linreg:  # Linear Regression
         linreg = LinearRegression()
         linreg.fit(log_f.reshape(-1, 1), log_psd.reshape(-1, 1))
-        print('Torbjørn\nintercept: ', linreg.intercept_, '\nslope: ', linreg.coef_)
+        print('Torbjørn\nintercept: ', linreg.intercept_,
+              '\nslope: ', linreg.coef_)
 
 
 def psd_graity(do_linreg=False):
@@ -30,10 +31,11 @@ def psd_graity(do_linreg=False):
     if do_linreg:  # Linear Regression
         linreg = LinearRegression()
         linreg.fit(log_f[1:].reshape(-1, 1), log_psd[1:].reshape(-1, 1))
-        print('Graity\nintercept: ', linreg.intercept_, '\nslope: ', linreg.coef_)
+        print('Graity\nintercept: ', linreg.intercept_,
+              '\nslope: ', linreg.coef_)
 
 
-def psd_from_baranauskas2012():
+def psd_from_baranauskas2012(do_linreg=False):
     # logX1,logY1,logX2,Y2
     filepath = "Data_PSD_other/psd_Baranbuskas_Fig1C.csv"
     data = pd.read_csv(filepath, usecols=['logX1', 'logY1'])
@@ -41,42 +43,39 @@ def psd_from_baranauskas2012():
     log_psd = np.log10(data['logY1'].values / 1000 ** 2)  # from micro to milli
     plt.plot(log_frequency, log_psd, color='darkolivegreen',
              label='LFP-Baranauskas')
-    linreg = LinearRegression()
-    linreg.fit(log_frequency.reshape(-1, 1), log_psd.reshape(-1, 1))
-    print('Baranauskas\nintercept: ', linreg.intercept_, '\nslope: ', linreg.coef_)
+    if do_linreg:  # Linear Regression
+        linreg = LinearRegression()
+        linreg.fit(log_frequency.reshape(-1, 1), log_psd.reshape(-1, 1))
+        print('Baranauskas\nintercept: ', linreg.intercept_,
+              '\nslope: ', linreg.coef_)
 
 
-def psd_from_milstein2009():
-    # WRONG, PS not PSD
-    # X,Y
-    filepath = "Data_PSD_other/psd_Milstein2009_Fig1.csv"
-    data = pd.read_csv(filepath, usecols=['X', 'Y'])
-    log_frequency = np.log10(data['X'].values)
-    log_psd = np.log10(data['Y'].values / 1000 ** 2)  # from micro to milli
-    plt.plot(log_frequency, log_psd, label='LFP-Milstein2009')
-
-
-def psd_from_jankowski2017():
+def psd_from_jankowski2017(do_linreg=False):
     filepath = "Data_PSD_other/psd_Jankowski2017_Fig2F.csv"
     data = pd.read_csv(filepath, usecols=['X', 'Y'])
     log_frequency = np.log10(data['X'].values)
     log_psd = np.log10(data['Y'].values / 1000 ** 2)  # from micro to milli
     plt.plot(log_frequency, log_psd, color='goldenrod', label='LFP-Jankowski')
-    linreg = LinearRegression()
-    linreg.fit(log_frequency[:-4].reshape(-1, 1), log_psd[:-4].reshape(-1, 1))
-    print('Jankowski\nintercept: ', linreg.intercept_, '\nslope: ', linreg.coef_)
+    if do_linreg:  # Linear Regression
+        linreg = LinearRegression()
+        linreg.fit(log_frequency[:-4].reshape(-1, 1),
+                   log_psd[:-4].reshape(-1, 1))
+        print('Jankowski\nintercept: ', linreg.intercept_,
+              '\nslope: ', linreg.coef_)
 
 
-def psd_from_miller2009():
+def psd_from_miller2009(do_linreg=False):
     # filepath = "Data_PSD_other/psd_Miller2009_Fig2A.csv"
     filepath = "Data_PSD_other/Miller2009_Fig2A_28.04.csv"
     data = pd.read_csv(filepath, usecols=['X', 'Y'])
     log_frequency = np.log10(data['X'].values)
     log_psd = np.log10(data['Y'].values / 1000 ** 2)  # from micro to milli
     plt.plot(log_frequency, log_psd, color='sienna', label='LFP-Miller')
-    linreg = LinearRegression()
-    linreg.fit(log_frequency.reshape(-1, 1), log_psd.reshape(-1, 1))
-    print('Miller\nintercept: ', linreg.intercept_, '\nslope: ', linreg.coef_)
+    if do_linreg:  # Linear Regression
+        linreg = LinearRegression()
+        linreg.fit(log_frequency.reshape(-1, 1), log_psd.reshape(-1, 1))
+        print('Miller\nintercept: ', linreg.intercept_,
+              '\nslope: ', linreg.coef_)
 
 
 if __name__ == '__main__':
@@ -85,16 +84,15 @@ if __name__ == '__main__':
 
     psd_torbjorn(do_linreg=True)
     psd_graity(do_linreg=True)
-    psd_from_baranauskas2012()
-    #psd_from_milstein2009()  # This is power spectrum (PS) not PSD
-    psd_from_jankowski2017()
-    psd_from_miller2009()
+    psd_from_baranauskas2012(do_linreg=True)
+    psd_from_jankowski2017(do_linreg=True)
+    psd_from_miller2009(do_linreg=True)
 
     plt.title('PSDs of LFPs')
     plt.xlabel('log$_{10}$(frequency) [Hz]')
     plt.ylabel('log$_{10}$(PSD) [mV$^{2}$/Hz]')
     plt.xlim([-2, 3.1])
-    plt.ylim([-10, 1.2])   # plt.ylim([-10, 2.2])  # with milstein
+    plt.ylim([-10, 1.2])
     plt.legend(loc="lower left")
     plt.savefig('Figures/psd_from_figures+.pdf', dpi=500, bbox_inches='tight')
     plt.show()
